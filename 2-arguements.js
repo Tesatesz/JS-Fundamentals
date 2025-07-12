85de0b69d1ce313ff7e6abe0821fceae9c3a071e
mukumuku Theresa <mukumukutheresa@gmail.com>
2025-07-12 23:49:35 +0100

    2-arguments.js

--- 2-arguements.js
+++ 2-arguements.js
@@ -1,11 +1,11 @@
-const args = process.argv;
-const sizeOfArguments = args.length;
+const args = process.argv.slice(2);
 
-if (sizeOfArguments <= 2) {
-  console.log("No argument");
-} else if (sizeOfArguments === 3) {
-  console.log("Argument found");
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
+
 
